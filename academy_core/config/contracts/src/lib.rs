efea8c9c54424dbd80d0f5aec41b0a06b8b412dd
Defelo <mail@defelo.de>
2024-09-28 14:46:57 +0200

    docs(academy_core_config): add docs

--- academy_core/config/contracts/src/lib.rs
+++ academy_core/config/contracts/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg_attr(feature = "mock", mockall::automock)]
 pub trait ConfigFeatureService: Send + Sync + 'static {
+    /// Return the public reCAPTCHA sitekey if reCAPTCHA is enabled.
     #[allow(
         clippy::needless_lifetimes,
         reason = "explicit lifetime needed for automock"
